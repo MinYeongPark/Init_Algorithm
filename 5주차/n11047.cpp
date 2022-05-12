d9dbd2db3557441ba824529e977414052bf058cc
MinYeongPark <yingo24655@naver.com>
2022-05-12 14:42:51 +0900

    [ADD] 백준 11047번 풀이

---
+++ 5주차/n11047.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, K;
+	scanf("%d %d", &N, &K);
+	
+	int* arr = new int[N];
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	int count = 0;
+	for (int i = N - 1; i >= 0; i--) {
+		count += K / arr[i];
+		K = K % arr[i];
+	}
+	printf("%d", count);
+	return 0;
+}
